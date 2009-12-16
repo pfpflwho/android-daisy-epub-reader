10d84a72b6c0569dfb99f04e8018129bacb05105
Jiayao.Yu <Jiayao.Yu@0d8aedb4-d056-11de-90e1-e379c62a6cce>
2009-12-16 14:58:10 +0000

    recover a couple of files deleted accidentally in r50

---
+++ src/com/ader/SmilFile.java
@@ -0,0 +1,54 @@
+package com.ader;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import android.util.Log;
+
+public class SmilFile implements Serializable {
+	private static final String TAG = "SimlFile";
+	private String fileName;
+	private ArrayList<SmilEntry> audioSegments = new ArrayList<SmilEntry>(); 
+	private ArrayList<SmilEntry> textSegments = new ArrayList<SmilEntry>();
+	
+	public String getFilename() {
+		return this.fileName;
+	}
+
+	public void open(String filename) {
+		Log.i(TAG, "Open " + filename);
+		clear();
+		this.fileName = filename;
+		DaisyParser parser = new DaisyParser();
+		String elementName;
+		
+		ArrayList<DaisyElement> elements = parser.parse(filename);
+		
+		for (int i = 0; i < elements.size(); i++) {
+			elementName = elements.get(i).getName();
+			SmilEntry entry = null;
+			if (elementName.equalsIgnoreCase("audio")) {
+			    entry = new SmilEntry(elements.get(i));
+				audioSegments.add(entry);
+			} else if (elementName.equalsIgnoreCase("text")){
+				DaisyElement name = elements.get(i);
+				entry = new SmilEntry(name);
+				textSegments.add(entry);
+			}
+			Log.i(TAG, "Adding segment " + entry);
+		}
+	}
+	
+	public ArrayList<SmilEntry> getAudioSegments() {
+		return audioSegments;
+	}
+	
+	public ArrayList<SmilEntry> getTextSegments() {
+		return textSegments;
+	}
+	
+	public void clear() {
+		audioSegments.clear();
+		textSegments.clear();
+	}
+}
