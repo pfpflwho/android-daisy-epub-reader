b6a91dca09bd29ed7c7bb098b1cbcc5b8e063bbe
julianharty <julianharty@0d8aedb4-d056-11de-90e1-e379c62a6cce>
2009-12-20 19:12:43 +0000

    Improved the TextLocator to extract the text from child elements e.g. a paragraph of text. Added a couple of large unit tests which extract a sentence and a paragraph from  a sample DAISY 202 book from daisy.org. I've also checked in that book's contents to make the tests self-contained. Note: this book is a text-only DAISY book.

--- src/com/ader/smil/TextLocator.java
+++ src/com/ader/smil/TextLocator.java
@@ -1,16 +1,18 @@
 package com.ader.smil;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.logging.Logger;
 
-import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
 import org.xml.sax.Attributes;
+import org.xml.sax.EntityResolver;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.DefaultHandler;
 
 /**
@@ -22,7 +24,7 @@ public class TextLocator extends DefaultHandler {
     private File baseDirectory;
     private String targetId;
     private String result;
-    private boolean found;
+    private int depth = 0;
     private Logger log = Logger.getAnonymousLogger();
     
     public TextLocator(File baseDirectory) {
@@ -30,15 +32,37 @@ public class TextLocator extends DefaultHandler {
     }
     
     public String getText(String src) throws IOException {
-        found = false;
         if (src.contains("#")) {
             targetId = src.substring(src.indexOf("#") + 1);
             File file = new File(baseDirectory, src.substring(0, src.indexOf("#")));
             SAXParserFactory factory = SAXParserFactory.newInstance();
             try {
-                SAXParser parser = factory.newSAXParser();
-                org.xml.sax.InputSource input = new InputSource(new FileInputStream(file));
-                parser.parse(input, this);
+            	org.xml.sax.InputSource input = new InputSource(new FileInputStream(file));
+            	XMLReader saxParser = factory.newSAXParser().getXMLReader();
+            	
+            	// The EntityResolver prevents the SAX parser from trying to
+            	// download external entities e.g. xhtml1-strict.dtd from
+            	// the referenced URI. Having our own entity resolver makes
+            	// the tests both faster, as they don't need to visit the web;
+            	// and more reliable, as the w3c site returns a HTTP 503 to
+            	// requests for this file from the SAX parser (it loads OK in
+            	// a web browser).
+            	// Thanks to: http://forums.sun.com/thread.jspa?threadID=413937
+            	// for the following code and fix.
+            	saxParser.setEntityResolver(new EntityResolver() {
+            		public InputSource resolveEntity(String publicId, String systemId)
+            		throws SAXException, java.io.IOException
+            		{
+            			if (systemId.endsWith(".dtd"))
+            			{
+            				return new InputSource(new ByteArrayInputStream(
+            						"<?xml version='1.0' encoding='UTF-8'?>".getBytes()));
+            			}
+            			else return null;
+            		}
+            	});
+            	saxParser.setContentHandler(this);
+            	saxParser.parse(input);
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
@@ -60,18 +84,24 @@ public class TextLocator extends DefaultHandler {
         String id = attributes.getValue("id");
         log.info(name + ": " + id);
         if (id != null && id.equals(targetId)) {
-            found = true;
+            depth++;
         }
     }
 
     @Override
     public void characters(char[] ch, int start, int length)
             throws SAXException {
-        if (found) {
+        if (depth > 0) {
             result = new String(ch, start, length);
             log.info("found " + result);
-            found = false;
         }
+    }
+    
+    @Override
+    public void endElement(String uri, String localName, String name) {
+    	if (depth > 0) {
+    		depth--;
+    	}
     }
     
 }
--- tests/com/ader/smil/TextLocatorTest.java
+++ tests/com/ader/smil/TextLocatorTest.java
@@ -6,18 +6,39 @@ import java.io.IOException;
 import junit.framework.TestCase;
 
 public class TextLocatorTest extends TestCase {
-    private TextLocator textLocator;
+    private TextLocator generalTextLocator;
+    private TextLocator wipoTextLocator;
     
     public void setUp() {
-        textLocator = new TextLocator(new File("./Resources"));
+        generalTextLocator = new TextLocator(new File("./Resources"));
+        wipoTextLocator = new TextLocator(new File("./Resources/WIPO-Treaty-D202Fileset"));
     }
     
     public void testLoadFromFile() throws IOException {
-        assertEquals("test1\ntest2\ntest3\n", textLocator.getText("test.txt"));
+        assertEquals("test1\ntest2\ntest3\n", generalTextLocator.getText("test.txt"));
     }
     
     public void testLoadFromTag() throws IOException {
-        assertEquals("Hello World", textLocator.getText("test.xml#001"));
-        assertEquals("Test", textLocator.getText("test.xml#002"));
+        assertEquals("Hello World", generalTextLocator.getText("test.xml#001"));
+        assertEquals("Test", generalTextLocator.getText("test.xml#002"));
+    }
+    
+    public void testLoadFromWipoDaisy202Book() throws IOException {
+    	// TODO: Decide how to handle tabs, new line characters, etc.
+    	assertEquals("Nature and Scope of Obligations \n",
+    			wipoTextLocator.getText("WIPOTreatyForVisuallyImpaired.html#id_95"));
+    	/**
+    	 * We need to ensure TextLocator traverses into child elements;
+    	 * In the following case (id_92) the body of the text is in an 'a' element.
+    	 */
+    	String paragraph = "The purpose of this Treaty is to provide the " +
+    		"necessary minimum flexibilities in copyright laws that are needed to ensure full and equal access to information and "+
+    		"communication for persons who are visually impaired or otherwise disabled in terms of reading copyrighted works, focusing in " +
+    		"particular on measures that are needed to publish and distribute works in formats that are accessible for persons who are " +
+    		"blind, have low vision, or have other disabilities in reading text, in order to support their full and effective participation " +
+    		"in society on an equal basis with others, and to ensure the opportunity to develop and utilize their creative, artistic and " +
+    		"intellectual potential, not only for their own benefit, but also for the enrichment of society. ";
+    	assertEquals(paragraph, 
+    		wipoTextLocator.getText("WIPOTreatyForVisuallyImpaired.html#id_92"));
     }
 }
