34e15034c4b6b5a9599bdb8751986ac6937fffcb
julianharty <julianharty@0d8aedb4-d056-11de-90e1-e379c62a6cce>
2009-12-23 18:41:56 +0000

    Added first test for DaisyBook, and a workaround so the SAX parser can open nested files with relative paths.

--- trunk/src/com/ader/DaisyParser.java
+++ trunk/src/com/ader/DaisyParser.java
@@ -1,17 +1,25 @@
 package com.ader;
 
 import org.xml.sax.Attributes;
+import org.xml.sax.EntityResolver;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.DefaultHandler;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
 public class DaisyParser extends DefaultHandler {
+	private static final String TAG = DaisyParser.class.getSimpleName();
 	ArrayList<DaisyElement> daisyElements = new ArrayList<DaisyElement>();
 	DaisyElement current;
 	private StringBuilder builder = new StringBuilder();
@@ -33,17 +41,46 @@ public class DaisyParser extends DefaultHandler {
 		// 
 	}
 
-	public ArrayList<DaisyElement> parse(String XMLFile) {
+	public ArrayList<DaisyElement> parse(final String XMLFile) {
+		Util.logInfo(TAG, "XMLFILE" + XMLFile);
 		SAXParserFactory factory = SAXParserFactory.newInstance();
 		try {
 			SAXParser parser = factory.newSAXParser();
 
 			org.xml.sax.InputSource input = new InputSource(new FileReader(
 					XMLFile));
-
-			parser.parse(input, this);
+			XMLReader saxParser = factory.newSAXParser().getXMLReader();
+        	// The EntityResolver ensures the SAX parser can correctly locate
+			// referenced entities e.g. xhtml1-strict.dtd from
+        	// the referenced URI when the files are in relative paths. 
+			// 
+        	// Thanks to: http://forums.sun.com/thread.jspa?threadID=413937 and
+			// http://www.jdom.org/pipermail/jdom-interest/2001-August/007129.html
+        	// which helped us to resolve the problem.
+			
+			// TODO: We may need to limit this handler to file:// references and
+			// find another way of resolving other references e.g. http://
+        	saxParser.setEntityResolver(new EntityResolver() {
+        		public InputSource resolveEntity(String publicId, String systemId)
+        		throws java.io.IOException
+        		{
+        			String directory = XMLFile.substring(0, XMLFile.lastIndexOf("/") + 1);
+        			Util.logInfo(TAG, "xml directory:" + directory);
+    				String resourcePath =
+    					directory + systemId.substring(systemId.lastIndexOf("/") + 1);
+					return new InputSource(
+    						new BufferedReader(new FileReader(resourcePath)));
+        		}
+        	});
+        	saxParser.setContentHandler(this);
+        	saxParser.parse(input);
+			// parser.parse(input, this);
 			return daisyElements;
-		} catch (Exception e) {
+		} catch (SAXException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} catch (ParserConfigurationException e) {
 			throw new RuntimeException(e);
 		}
 	}
