b4deabbb6f46f04ad0dfcd3eed1916c75a9e6625
julianharty <julianharty@0d8aedb4-d056-11de-90e1-e379c62a6cce>
2010-06-24 12:12:39 +0000

    Deleting another old folder.

--- src/com/ader/HomeScreen.java
+++ src/com/ader/HomeScreen.java
@@ -1,6 +1,7 @@
 package com.ader;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -23,6 +24,27 @@ public class HomeScreen extends Activity implements OnClickListener {
     	case R.id.open_button:
     		Intent i = new Intent(this, DaisyBrowser.class);
     		startActivity(i);
+    		break;
+    	
+    	case R.id.search_button:
+    		// TODO (jharty): there are disconnects between the name of this
+    		// button and the Intent we're calling (which doesn't allow the
+    		// user to search directly. I think this code will end up being
+    		// called by the open button, replacing the current, crude open
+    		// implementation. However I'll work here until the new code has
+    		// been integrated.
+    		Intent iSearch = new Intent(this, DaisyBookFinder.class);
+    		startActivity(iSearch);
+    		break;
+    		
+    	case R.id.help_button:
+			AlertDialog.Builder builder=new AlertDialog.Builder(this);
+			
+			builder
+				.setTitle(R.string.player_instructions_description)
+				.setMessage(R.string.player_instructions)
+				.setPositiveButton(R.string.close_instructions, null)
+				.show();
     		break;
     	}
     }
