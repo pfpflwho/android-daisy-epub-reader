3ff6ed91830b604ad6cfea2e20726046d5d9d2d8
julianharty <julianharty@0d8aedb4-d056-11de-90e1-e379c62a6cce>
2010-07-18 19:48:34 +0000

    Added the ability to add different valid and invalid levels to a generated Daisy 2.02 ebook structure. Also added a basic test, which doesn't check the content in any depth. We'll do so in a later revision.

--- tests/com/ader/testutilities/CreateEBookTest.java
+++ tests/com/ader/testutilities/CreateEBookTest.java
@@ -5,10 +5,16 @@ import java.io.ByteArrayOutputStream;
 import junit.framework.TestCase;
 
 public class CreateEBookTest extends TestCase {
-
+	private ByteArrayOutputStream out;
+	private CreateDaisy202Book eBook=null;
+	
+	@Override
+	public void setUp() throws Exception {
+		out = new ByteArrayOutputStream();
+		eBook = new CreateDaisy202Book(out);
+	}
+	
 	public void testCreateMinimalDaisy202Book() throws Exception {
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		CreateDaisy202Book eBook = new CreateDaisy202Book(out);
 		eBook.writeXmlHeader();
 		eBook.writeDoctype();
 		eBook.writeXmlns();
@@ -18,6 +24,28 @@ public class CreateEBookTest extends TestCase {
 		assertTrue("There should be some content", out.size() > 50);
 		System.out.println(out.toString());
 		// TODO(jharty): We could validate the XML here too...
+	}
+
+	/**
+	 * Simply generate an unrealistic book with all the valid levels.
+	 */
+	public void testCreateARoccocoDaisy202Book() {
+		eBook.writeXmlHeader();
+		eBook.writeDoctype();
+		eBook.writeXmlns();
+		eBook.writeBasicMetadata();
+		eBook.addLevelOne();
+		eBook.addLevelFive();
+		eBook.addLevelFour();
+		eBook.addLevelSix();
+		eBook.addLevelThree();
+		eBook.addLevelTwo();
+		eBook.addLevelFive();
+		eBook.addLevelFive();
+		eBook.addLevelOne();
+		eBook.writeEndOfDocument();
+		assertTrue("There should be some content", out.size() > 50);
+		System.out.println(out.toString());
 	}
 
 }
