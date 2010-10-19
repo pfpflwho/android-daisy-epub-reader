7b6e27f65591c729c7b93f05da2100648c4056aa
julianharty <julianharty@0d8aedb4-d056-11de-90e1-e379c62a6cce>
2010-10-19 08:57:03 +0000

    I've hopefully managed to add support for books in encodings such as iso-8859-1 e.g. some books from ONCE in Spain. New test class added to text the regular expressions used.

--- src/com/ader/DaisyPlayer.java
+++ src/com/ader/DaisyPlayer.java
@@ -160,7 +160,8 @@ public class DaisyPlayer extends Activity implements OnCompletionListener {
 			} catch (IllegalStateException e) {
 				throw new RuntimeException(e);
 			} catch (IOException e) {
-				throw new RuntimeException(e);
+				throw new RuntimeException(bookmark.getFilename() 
+						+ "\n" + e.getLocalizedMessage());
 			}
 			player.seekTo(bookmark.getPosition());
 			player.start();
--- src/com/ader/SmilFile.java
+++ src/com/ader/SmilFile.java
@@ -1,10 +1,13 @@
 package com.ader;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -16,6 +19,10 @@ import com.ader.smil.SmilParser;
 import com.ader.smil.TextElement;
 
 public class SmilFile implements Serializable {
+	protected static final String XML_TRAILER = "\"?>";
+	protected static final String EXTRACT_ENCODING_REGEX = ".*encoding=\"";
+	protected static final String XML_FIRST_LINE_REGEX = "<\\?xml version=\"1\\.0\" encoding=\"(.*)\"?>";
+	
 	private String fileName;
 	private SequenceElement elements;
 	
@@ -24,8 +31,24 @@ public class SmilFile implements Serializable {
 	}
 
 	public void open(String filename) throws FileNotFoundException, IOException {
+		String encoding = "UTF-8";
            try {
-			elements = new SmilParser().parse(new FileInputStream(filename));
+			FileInputStream fis = new FileInputStream(filename);
+			BufferedInputStream bis = new BufferedInputStream(fis);
+			if (bis.markSupported()) {
+				String line = null;
+				// read the first line after setting the mark, then reset
+				// before calling the parser.
+				bis.mark(200);
+				DataInputStream dis = new DataInputStream(bis);
+				line = dis.readLine();
+				if (line.matches(SmilFile.XML_FIRST_LINE_REGEX)) {
+					encoding = extractEncoding(line);
+				}
+				bis.reset();
+			}
+			
+			elements = new SmilParser().parse(bis, encoding);
 		} catch (SAXException e) {
 			throw new RuntimeException("Problem with file: " + filename 
 					+ "\n" + e.getLocalizedMessage());
@@ -41,5 +64,17 @@ public class SmilFile implements Serializable {
 	
 	public List<TextElement> getTextSegments() {
 		return elements.getAllTextElementDepthFirst();
+	}
+	
+	/**
+	 * Helper method to extract the XML file encoding
+	 * @param line the first line of an XML file
+	 * @return The value of the encoding in lower-case.
+	 */
+	protected static String extractEncoding(String line) {
+		Pattern p = Pattern.compile(EXTRACT_ENCODING_REGEX);
+		String matches[] = p.split(line);
+		String encoding = matches[1].replace(XML_TRAILER, "");
+		return encoding.toLowerCase();
 	}
 }
