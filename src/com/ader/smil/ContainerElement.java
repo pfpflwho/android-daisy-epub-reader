ba9dac34c470da248735d558d800fc9cfa0179ca
julianharty <julianharty@0d8aedb4-d056-11de-90e1-e379c62a6cce>
2010-12-29 17:55:02 +0000

    Adding the simplest assertTrue(true) activity test case as the first step in experimenting with using Robotium http://code.google.com/p/robotium/ for automated testing of this DaisyReader application.

--- src/com/ader/smil/ContainerElement.java
+++
@@ -1,8 +0,0 @@
-package com.ader.smil;
-
-import java.util.List;
-
-public interface ContainerElement extends SmilElement {
-    public List<AudioElement> getAllAudioElementDepthFirst();
-    public List<TextElement> getAllTextElementDepthFirst();
-}
--- src/com/ader/smil/SmilElement.java
+++
@@ -1,4 +0,0 @@
-package com.ader.smil;
-
-public interface SmilElement {
-}
