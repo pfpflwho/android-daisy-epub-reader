5a40cd21377bc1cd0f4b6e0dcebc81807aa5531b
julianharty <julianharty@0d8aedb4-d056-11de-90e1-e379c62a6cce>
2010-12-29 18:33:17 +0000

    Robotium Test Automation: Adding basic calls to robotium.solo(). No Robotium Tests yet.

--- DaisyReaderTest/src/com/ader/test/ExampleTest.java
+++ DaisyReaderTest/src/com/ader/test/ExampleTest.java
@@ -3,12 +3,29 @@ package com.ader.test;
 import android.test.ActivityInstrumentationTestCase2;
 
 import com.ader.DaisyReader;
+import com.jayway.android.robotium.solo.Solo;
 
 public class ExampleTest extends ActivityInstrumentationTestCase2<DaisyReader> {
+	private Solo solo;
 
 	public ExampleTest() {
 		super("com.ader", DaisyReader.class);
-		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	protected void setUp() throws Exception {
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		try {
+			solo.finalize();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		getActivity().finish();
+		super.tearDown();
 	}
 
 	public void testTrue() {
