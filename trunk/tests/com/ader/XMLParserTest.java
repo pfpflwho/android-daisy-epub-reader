01e08294aef28009de6b1bb4b315f67415cba556
julianharty <julianharty@0d8aedb4-d056-11de-90e1-e379c62a6cce>
2011-01-16 09:06:00 +0000

    restoring 4 java files containing tests. Thanks to Ole for pointing out the problem.

---
+++ trunk/tests/com/ader/XMLParserTest.java
@@ -0,0 +1,59 @@
+package com.ader;
+/**
+ * Tests for the XML Parser class.
+ */
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+import com.ader.testutilities.CreateDaisy202Book;
+import com.ader.testutilities.SampleContent;
+
+import junit.framework.TestCase;
+
+public class XMLParserTest extends TestCase {
+	private NavCentre navCenter = null;
+	
+	@Override
+	public void setUp() {
+		ByteArrayInputStream input =
+			new ByteArrayInputStream((SampleContent.simpleValidNccHtml).getBytes());
+		XMLParser parser = new XMLParser(input);
+		navCenter = parser.processNCC();
+	}
+	
+	public void testXMLParserLoadsValidDaisy202Content() {
+		String firstTitleFound = navCenter.getNavPoint(0).getText();
+		System.out.println("The first value of the navcenter is: " + firstTitleFound);
+		assertEquals("The first title should match", SampleContent.firstTitle, firstTitleFound);
+	}
+	
+	public void testWhatHappensIfWeAskForContentBeyondTheArray() {
+		int outOfRange = navCenter.count() + 1;
+		NavPoint n = navCenter.getNavPoint(outOfRange);
+		assertNull("Asking for an item beyond the end of the book, should return null", n);
+	}
+	
+	public void testWhatHappensIfWeAskForANegativeIndex() {
+		NavPoint n = navCenter.getNavPoint(-1);
+		assertNull("Asking for a negative index should return null", n);
+	}
+	
+	public void testALargerGeneratedBookContainsTheExpectedSections() throws Exception {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		CreateDaisy202Book eBook = new CreateDaisy202Book(out);
+		eBook.writeXmlHeader();
+		eBook.writeDoctype();
+		eBook.writeXmlns();
+		eBook.writeBasicMetadata();
+		int numLevelOneSections = 10;
+		for (int i = 1; i <= numLevelOneSections; i++) {
+			eBook.addLevelOne();
+		}
+		eBook.writeEndOfDocument();
+		ByteArrayInputStream newBook = new ByteArrayInputStream(out.toByteArray());
+		XMLParser anotherParser = new XMLParser(newBook);
+		NavCentre nc = anotherParser.processNCC();
+		assertEquals("Expected a 1:1 match of NavPoints and sections", 
+				numLevelOneSections, nc.count());
+	}
+}
