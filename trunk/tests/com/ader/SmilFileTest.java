3219a190c8b9797a4e84c8765cbfa1319fa039b1
julianharty@gmail.com <julianharty@gmail.com@0d8aedb4-d056-11de-90e1-e379c62a6cce>
2011-06-05 21:58:27 +0000

    Adding the initial translation of the messages into Swedish. Thank you to Raouf for these. We will refine them as part of a general effort to make the software clearer and easier to use. Note: I have not tested these translations yet, so we may need to make some corrections during the testing.

--- trunk/tests/com/ader/SmilFileTest.java
+++ trunk/tests/com/ader/SmilFileTest.java
@@ -1,11 +1,17 @@
 package com.ader;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import junit.framework.TestCase;
+
+import android.test.suitebuilder.annotation.MediumTest;
 
 import com.ader.smil.TextElement;
 
 public class SmilFileTest extends TestCase {
 
+	@MediumTest
     public void testParsingTextOnlyFile() throws Exception {
         SmilFile smilFile = new SmilFile();
         smilFile.open("Resources/WIPO-Treaty-D202Fileset/d202_1.smil");
@@ -13,7 +19,17 @@ public class SmilFileTest extends TestCase {
         TextElement textElement = smilFile.getTextSegments().get(0);
         assertEquals("WIPOTreatyForVisuallyImpaired.html#id_4", textElement.getSrc());
     }
-    
+
+	@MediumTest
+	public void testExtractingTextFromTextElement() throws Exception {
+	SmilFile smilFile = new SmilFile();
+	smilFile.open("Resources/WIPO-Treaty-D202Fileset/d202_4.smil");	
+	assertEquals("Expected 2 text segments in d202_4.smil", 2, smilFile.getTextSegments().size());
+	TextElement textElement = smilFile.getTextSegments().get(1);
+	assertEquals("WIPOTreatyForVisuallyImpaired.html#id_92", textElement.getSrc());
+	}
+	
+	@MediumTest
     public void testParsingAudioOnlyFile() throws Exception {
         SmilFile smilFile = new SmilFile();
         smilFile.open("Resources/light-man/icth0001.smil");
@@ -23,6 +39,7 @@ public class SmilFileTest extends TestCase {
         assertEquals(smilFile.getAudioSegments().get(2).getClipBegin(), 4.441);
     }
     
+	@MediumTest
     public void testParsingFileWithWindows1252Encoding() throws Exception {
     	SmilFile smilfile = new SmilFile();
     	smilfile.open("Resources/problemcontent/bcbw0001.smil");
@@ -30,6 +47,7 @@ public class SmilFileTest extends TestCase {
     			9, smilfile.getAudioSegments().size());
     }
     
+	@MediumTest
     public void testParsingCBFW000BWithWindows1252Encoding() throws Exception {
     	SmilFile smilfile = new SmilFile();
     	smilfile.open("Resources/problemcontent/cbfw000B.smil");
@@ -39,4 +57,5 @@ public class SmilFileTest extends TestCase {
     	SmilFile smilfile2 = new SmilFile();
     	smilfile2.open("/sdcard/problemcontent/cbfw0029.smil");
     	
-    }}
+    }
+}
