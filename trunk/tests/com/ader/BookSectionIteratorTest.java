9d1e0f827e1490def6165a731f29acd334992926
julianharty <julianharty@0d8aedb4-d056-11de-90e1-e379c62a6cce>
2011-06-17 09:55:49 +0000

    Adding a new test class for the Book Section Iterator.

---
+++ trunk/tests/com/ader/BookSectionIteratorTest.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.ader;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Julian Harty
+ *
+ */
+public class BookSectionIteratorTest extends TestCase {
+
+	protected void setUp() {
+		
+	}
+	/**
+	 * Test method for {@link com.ader.BookSectionIterator#hasNext()}.
+	 */
+	public void testHasNext() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.ader.BookSectionIterator#next()}.
+	 */
+	public void testNext() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.ader.BookSectionIterator#remove()}.
+	 */
+	public void testRemove() {
+		fail("Not yet implemented");
+	}
+
+}
