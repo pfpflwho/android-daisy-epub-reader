dac89e8b998184908ab034f96666a445d8135b74
julianharty <julianharty@0d8aedb4-d056-11de-90e1-e379c62a6cce>
2011-06-17 12:44:27 +0000

    Adding tests for the addTheseLevels method in CreateDaisy202Book. These tests are prerequisites for additional tests for the book navigation code.

--- tests/com/ader/TestAbilityToGenerateDaisy202Book.java
+++ tests/com/ader/TestAbilityToGenerateDaisy202Book.java
@@ -2,10 +2,6 @@ package com.ader;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
 import junit.framework.TestCase;
@@ -43,8 +39,7 @@ public class TestAbilityToGenerateDaisy202Book extends TestCase {
 		eBook.addSmilFileEntry(1, "", "");
 		eBook.writeEndOfDocument();
 
-		ByteArrayInputStream newBook = new ByteArrayInputStream(out.toByteArray());
-		XMLParser anotherParser = new XMLParser(newBook);
+		XMLParser anotherParser = createDaisyBookIndex();
 		
 		NavCentre nc = anotherParser.processNCC();
 		assertEquals("Expected a 1:1 match of NavPoints and sections", 0, nc.count());
@@ -60,10 +55,56 @@ public class TestAbilityToGenerateDaisy202Book extends TestCase {
 		eBook.addSmilFileEntry(1, "singleEntry.smil", id);
 		eBook.writeEndOfDocument();
 
-		ByteArrayInputStream newBook = new ByteArrayInputStream(out.toByteArray());
-		XMLParser anotherParser = new XMLParser(newBook);
+		XMLParser anotherParser = createDaisyBookIndex();
 		
 		NavCentre nc = anotherParser.processNCC();
 		assertEquals("Expected a 1:1 match of NavPoints and sections", 1, nc.count());
-	}	
+	}
+	
+	@SmallTest
+	public void testAbilityToAddVariousValidLevels() {
+		final String sections = "123456";
+		eBook.addTheseLevels(sections);
+		eBook.writeEndOfDocument();
+		
+		XMLParser anotherParser = createDaisyBookIndex();
+		NavCentre nc = anotherParser.processNCC();
+		assertEquals(
+				String.format("Expected a section for each section added.", sections.length()),
+				sections.length(), nc.count());
+	}
+
+	@SmallTest
+	public void testExceptionThrownForInvalidLevels() {
+		final String sections = "1234567";
+		try {
+			eBook.addTheseLevels(sections);
+			fail(String.format("An exception should be thrown as %s contains an invalid level: 7",
+					sections));
+			
+		} catch (IllegalArgumentException iae) {
+			// pass, this is the expected result.
+		}
+	}
+	
+	@SmallTest
+	public void testExceptionThrownForInvalidContents() {
+		final String invalidContents = "abc";
+		try {
+			eBook.addTheseLevels(invalidContents);
+			fail(String.format("An exception should be thrown as %s contains invalid contents", 
+					invalidContents));
+		} catch (IllegalArgumentException iae) {
+			// pass, this is the expected result
+		}
+	}
+	/**
+	 * Simple helper method to reduce code duplication.
+	 * @return a parser that represents the index of a DAISY 2.02 book.
+	 */
+	private XMLParser createDaisyBookIndex() {
+		ByteArrayInputStream newBook = new ByteArrayInputStream(out.toByteArray());
+		XMLParser anotherParser = new XMLParser(newBook);
+		return anotherParser;
+	}
 }
