cd7dd0cec466bda7a129bc60467033b8b801d19c
julianharty@gmail.com <julianharty@gmail.com@0d8aedb4-d056-11de-90e1-e379c62a6cce>
2012-01-26 15:02:24 +0000

    Deleting junk svn / eclipse added.

--- src/org/androiddaisyreader/AudioCallbackListener.java
+++
@@ -1,5 +0,0 @@
-package org.androiddaisyreader;
-
-public interface AudioCallbackListener {
-	public void endOfAudio();
-}
--- src/org/androiddaisyreader/SnippetCallbackListener.java
+++
@@ -1,5 +0,0 @@
-package org.androiddaisyreader;
-
-public interface SnippetCallbackListener {
-	public void completedSection();
-}
