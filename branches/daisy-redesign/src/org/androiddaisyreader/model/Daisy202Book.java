7ce17b4897eb568f9433bb9b0fcb4b301687d46b
julianharty@gmail.com <julianharty@gmail.com@0d8aedb4-d056-11de-90e1-e379c62a6cce>
2012-01-26 15:46:49 +0000

    Refactored NCCSpecification to use recursion which cleans up the logic for handling the stack of sections.

---
+++ branches/daisy-redesign/src/org/androiddaisyreader/model/Daisy202Book.java
@@ -0,0 +1,53 @@
+package org.androiddaisyreader.model;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+public class Daisy202Book extends Book {
+	private Date date;
+
+	public static class Builder {
+		private Daisy202Book book = new Daisy202Book();
+
+		public Builder addSection(Section section) {
+			book.sections.add(section);
+			return this;
+		}
+		
+		public Builder setDate(Date date) {
+			book.date = date;
+			return this;
+		}
+
+		public Builder setTitle(String content) {
+			// TODO 20120124 (jharty): consider cleaning up the content.
+			book.title = content.trim();
+			return this;
+		}
+
+		public Daisy202Book build() {
+			book.sections = Collections.unmodifiableList(book.sections);
+			return book;
+		}
+	}
+
+	private NccSpecification ncc;
+	private Daisy202Book() {
+		super();
+	}
+	
+	public Navigable getParent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<? extends Navigable> getChildren() {
+		return sections;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+	
+}
