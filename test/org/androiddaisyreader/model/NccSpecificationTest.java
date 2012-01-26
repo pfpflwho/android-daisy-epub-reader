aa32fee4b8305562c41cf25068ff6d20bbc9bc38
julianharty@gmail.com <julianharty@gmail.com@0d8aedb4-d056-11de-90e1-e379c62a6cce>
2012-01-26 14:51:49 +0000

    Improved the tests for nested sections.

--- test/org/androiddaisyreader/model/NccSpecificationTest.java
+++ test/org/androiddaisyreader/model/NccSpecificationTest.java
@@ -1,8 +1,14 @@
 package org.androiddaisyreader.model;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
+
+import org.androiddaisyreader.testutilities.CreateDaisy202Book;
+import org.androiddaisyreader.testutilities.NotImplementedException;
+import org.androiddaisyreader.testutilities.SampleContent;
 
 import junit.framework.TestCase;
 
@@ -16,5 +22,33 @@ public class NccSpecificationTest extends TestCase {
 		
 		// TODO 201201 25 (jharty): the following test is ugly and uses a deprecated constructor.
 		assertEquals(new Date(2003 - 1900, 12 - 1, 24), thingy.getDate());
+	}
+	
+	public void testUsingValidSampleContent() throws IOException {
+		ByteArrayInputStream content = new ByteArrayInputStream((SampleContent.simpleValidNccHtml).getBytes());
+		Daisy202Book anotherThingy = NccSpecification.readFromStream(content, "utf-8");
+		assertEquals(SampleContent.firstTitle, anotherThingy.getTitle());
+		assertEquals(1, anotherThingy.sections.size());
+		assertEquals(1, anotherThingy.getChildren().size());
+	}
+	
+	public void testCorrectSectionsForTwoSectionContents() throws IOException {
+		ByteArrayInputStream content = new ByteArrayInputStream((SampleContent.validIcelandicNccHtml).getBytes("utf-8"));
+		Daisy202Book icelandicContents = NccSpecification.readFromStream(content, "utf-8");
+		assertEquals(2, icelandicContents.getChildren().size());
+	}
+	
+	public void testNestingOfSections() throws NotImplementedException, IOException {
+		ByteArrayOutputStream out  = new ByteArrayOutputStream();
+		CreateDaisy202Book eBookContents = new CreateDaisy202Book(out);
+		eBookContents.writeXmlHeader();
+		eBookContents.writeDoctype();
+		eBookContents.writeXmlns();
+		eBookContents.writeBasicMetadata();
+		eBookContents.addTheseLevels("12231");
+		eBookContents.writeEndOfDocument();
+		ByteArrayInputStream bookContents = new ByteArrayInputStream(out.toByteArray());
+		Daisy202Book book = NccSpecification.readFromStream(bookContents);
+		assertEquals("Count should match the number of level 1 sections.", 2, book.getChildren().size());
 	}
 }
