d068ea468e82ee14a9f4d6c73d0ed500398b2b46
julianharty@gmail.com <julianharty@gmail.com@0d8aedb4-d056-11de-90e1-e379c62a6cce>
2012-02-07 18:03:38 +0000

    Adding a simple command line utility to allow us to process arbitrary smil files.

---
+++ branches/daisy-redesign/test/org/androiddaisyreader/model/ProcessExternalSmilFile.java
@@ -0,0 +1,59 @@
+package org.androiddaisyreader.model;
+
+import static org.androiddaisyreader.model.XmlUtilities.obtainEncodingStringFromInputStream;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+public class ProcessExternalSmilFile {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		if (args.length == 0) {
+			printUsage("ProcessExternalSmilFile");
+			System.exit(1);
+		}
+		
+		InputStream contents = new FileInputStream(args[0]);
+		String encoding = obtainEncodingStringFromInputStream(contents);
+		Smil10Specification smil = new Smil10Specification();
+		
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		XMLReader saxParser;
+		try {
+			saxParser = factory.newSAXParser().getXMLReader();
+			saxParser.setEntityResolver(XmlUtilities.dummyEntityResolver());
+			saxParser.setContentHandler(smil);
+			InputSource input = new InputSource(contents);
+			input.setEncoding(encoding);
+			saxParser.parse(input);
+			Section section = smil.build();
+			// TODO 20120207 (jharty): consider checking the section contents here.
+			System.out.println("parsed file " + args[0] + " without error");
+			System.exit(0);
+		} catch (SAXException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+
+	}
+
+	private static void printUsage(String programName) {
+		System.out.println("Usage: \n\t" + programName + "smilfilename");
+	}
+
+}
