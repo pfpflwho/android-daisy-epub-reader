11a573e50140692db1331037e35d509c1c7c735a
julianharty@gmail.com <julianharty@gmail.com@0d8aedb4-d056-11de-90e1-e379c62a6cce>
2012-02-10 09:17:59 +0000

    Added another test for a SMIL file with a single audio element. Refactored the test class to reduce duplication by creating a private helper method. Beefed up both tests to ensure they don't have extraneous elements.
    Note: these tests are to help me decide how to complete the implementation of the model for SMIL files. The current model is incomplete as it only returns the first text element however we need to obtain all the text, etc.

--- test/org/androiddaisyreader/model/Smil10SpecificationTest.java
+++ test/org/androiddaisyreader/model/Smil10SpecificationTest.java
@@ -16,24 +16,58 @@ import org.xml.sax.XMLReader;
 import junit.framework.TestCase;
 
 public class Smil10SpecificationTest extends TestCase {
-	private static final String simpleSmil10Contents = 
+	private static final String SMIL10PREAMBLE = 
 		"<?xml version=\"1.0\" encoding=\"windows-1252\"?>" +
 		"<!DOCTYPE smil PUBLIC \"-//W3C//DTD SMIL 1.0//EN\" \"http://www.w3.org/TR/REC-smil/SMIL10.dtd\">" +
 		"<smil>" +
 		"<head>" +
 		"<meta name=\"dc:format\" content=\"Daisy 2.02\" />" +
 		"</head>" +
-		"<body>" +
-		"<seq dur=\"1.3s\">" +
+		"<body>";
+	private static final String SMIL10PROLOGUE = 
+		"</body>" +
+		"</smil>";
+	private static final String SMILWITH1TEXTSECTION = 
+		SMIL10PREAMBLE  +
+		"<seq dur=\"0.0s\">" +
 		"<par endsync=\"last\" id=\"par_12\">" +
 		"<text src=\"dummy.html#s8\" id=\"i10\" />" +
 		"</par>" +
 		"</seq>" +
-		"</body>" +
-		"</smil>";
+		SMIL10PROLOGUE;
+	private static final String SMILWITH1AUDIOSECTION = 
+		SMIL10PREAMBLE +
+		"<seq dur=\"1.4s\">" +
+		"<audio src=\"meow.mp3\" clip-begin=\"npt=0.000s\" clip-end=\"npt=1.666s\" id=\"audio_0001\"/>" +
+		"</seq>" +
+		SMIL10PROLOGUE;
 	
-	public void testParsingOfSimpleSmil10Structure() throws IOException, SAXException, ParserConfigurationException {
-		InputStream contents = new ByteArrayInputStream(simpleSmil10Contents.getBytes());
+	public void testParsingOfSimpleSmil10WithText() throws IOException, SAXException, ParserConfigurationException {
+		InputStream contents = new ByteArrayInputStream(SMILWITH1TEXTSECTION.getBytes());
+		Section section = parseSmilContents(contents);
+
+		assertEquals("Expected one part", 1, section.navigables.size());
+		Part part = (Part) section.navigables.get(0);
+		assertEquals("The part should contain one snippet", 1, part.getSnippets().size());
+		// TODO 20120207 revise once we implement processing of the snippets.
+		assertEquals("The snippet name is incorrect", "dummy.html#s8", part.getSnippets().get(0).getText());
+		assertEquals("Currently we expect only one snippet.", 1, part.getSnippets().size());
+		assertEquals("The part should not contain any audio elements", 0, part.getAudioElements().size());
+		}
+
+	public void testParsingOfSimpleSmil10WithAudio() throws IOException, SAXException, ParserConfigurationException {
+		InputStream contents = new ByteArrayInputStream(SMILWITH1AUDIOSECTION.getBytes());
+		Section section = parseSmilContents(contents);
+		assertEquals("Expected one part", 1, section.navigables.size());
+		Part part = (Part) section.navigables.get(0);		
+		assertEquals("The part should contain one audio element", 1, part.getAudioElements().size());
+		assertEquals("The part should not contain any snippets", 0, part.getSnippets().size());
+		
+	}
+	
+	
+	private Section parseSmilContents(InputStream contents) throws IOException,
+			SAXException, ParserConfigurationException {
 		String encoding = obtainEncodingStringFromInputStream(contents);
 		Smil10Specification smil = new Smil10Specification();
 		
@@ -45,15 +79,7 @@ public class Smil10SpecificationTest extends TestCase {
 		input.setEncoding(encoding);
 		saxParser.parse(input);
 		Section section = smil.build();
-		for (Navigable p : section.navigables) {
-			System.out.println(p.toString());
-		}
-		assertEquals("Expected one part", 1, section.navigables.size());
-		Part part = (Part) section.navigables.get(0);
-		assertEquals("The part should contain one snippet", 1, part.getSnippets().size());
-		// TODO 20120207 revise once we implement processing of the snippets.
-		assertEquals("The snippet name is incorrect", "dummy.html#s8", part.getSnippets().get(0).getText());
-		assertEquals("Currently we expect only one snippet.", 1, part.getSnippets().size());
+		return section;
 	}
 
 }
