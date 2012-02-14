5e0c6e9d9034b35734985b9b36bf51dfb36f97a3
julianharty@gmail.com <julianharty@gmail.com@0d8aedb4-d056-11de-90e1-e379c62a6cce>
2012-02-14 17:04:32 +0000

    Added several new files (including Daisy202Snippet I should have added earlier) and moved printUsage() to another new file CommandLineUtilities.
    We can now test whether a DAISY 2.02 book can be parsed sufficiently to extract the section details. They can :) In the process I discovered a bug in our assumptions and therefore our code. See http://code.google.com/p/android-daisy-epub-reader/issues/detail?id=54 for details.

---
+++ src/org/androiddaisyreader/model/Daisy202Snippet.java
@@ -0,0 +1,66 @@
+package org.androiddaisyreader.model;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class Daisy202Snippet extends Snippet {
+
+	private BookContext context;
+	private String uri;
+	private String id;
+
+	/**
+	 * Create a DAISY 2.02 snippet.
+	 * Uses the book's context & a composite reference.
+	 * 
+	 * The context may be a file location, an index into a zip file, etc. The
+	 * context is needed as composite references contain relative references.
+	 * 
+	 * A composite reference is formatted as follows:
+	 *   fire_safety.html#dol_1_4_rgn_cnt_0043
+	 *   An example of the reference in context follows:
+	 *   <text src="fire_safety.html#dol_1_4_rgn_cnt_0043" id="rgn_txt_0004_0017"/>
+	 * @param context
+	 * @param compositeReference
+	 */
+	Daisy202Snippet(BookContext context, String compositeReference) {
+		this.context = context;
+		String[] elements = compositeReference.split("#");
+		if (elements.length != 2) {
+			throw new IllegalArgumentException(
+					"Expected composite reference in the form uri#id, got " + compositeReference);
+		}
+		this.uri = elements[0];
+		this.id = elements[1];
+	}
+
+	/**
+	 * @return the text contents for this text element.
+	 */
+	@Override
+	public String getText() {
+		Document doc = null;
+		try {
+			InputStream contents = context.getResource(uri);
+			doc = Jsoup.parse(contents, context.getCharSet(uri), context.getBaseUri());
+			return doc.getElementById(id).text(); 
+		} catch (IOException ioe) {
+			// TODO 20120214 (jharty): we need to consider more appropriate error reporting.
+			throw new RuntimeException("TODO fix me", ioe);
+		}
+	}
+
+	/**
+	 * @return the id that points to the contents.
+	 */
+	public String getId() {
+		// TODO 20120214 (jharty): Consider keeping the composite reference as
+		// the ID since these IDs are only truly unique in the context of the
+		// file...
+		return id;
+	}
+
+}
---
+++ test/org/androiddaisyreader/model/CommandLineUtilities.java
@@ -0,0 +1,8 @@
+package org.androiddaisyreader.model;
+
+public class CommandLineUtilities {
+
+	static void printUsage(String programName) {
+		System.out.println("Usage: \n\t" + programName + " filename");
+	}
+}
