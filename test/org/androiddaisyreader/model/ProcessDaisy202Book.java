f4f75298e315fb9d681798d0621cf6922f3cc43e
julianharty@gmail.com <julianharty@gmail.com@0d8aedb4-d056-11de-90e1-e379c62a6cce>
2012-02-19 13:20:47 +0000

    Refactored the code so I can extract a method to open both filesystem and zipped books easily.

--- test/org/androiddaisyreader/model/ProcessDaisy202Book.java
+++ test/org/androiddaisyreader/model/ProcessDaisy202Book.java
@@ -9,8 +9,8 @@ import java.io.InputStream;
 
 public class ProcessDaisy202Book {
 
-	private static BookContext bookContext;
 	private static InputStream contents;
+	private static String encoding;
 
 	/**
 	 * @param args
@@ -21,26 +21,22 @@ public class ProcessDaisy202Book {
 			System.exit(1);
 		}
 		
-		StringBuilder filename = new StringBuilder();
+		StringBuilder filenameBuilder = new StringBuilder();
 		
 		// To help cope with spaces in the filename e.g. on my windows machine.
 		for (int i = 0; i < args.length; i++) {
-			filename.append(args[i]);
-		}
-		if (filename.toString().endsWith(".zip")) {
-			bookContext = new ZippedBookContext(filename.toString());
-			contents = bookContext.getResource("ncc.html");
-		} else {
-			contents = new FileInputStream(filename.toString());
-			String encoding = obtainEncodingStringFromInputStream(contents);
-	
-			File directory = new File(filename.toString());
-			bookContext = new FileSystemContext(directory.getParent());
-			directory = null;
+			filenameBuilder.append(args[i]);
 		}
 		
+		String filename = filenameBuilder.toString();
+		
+		BookContext bookContext = openBook(filename);
+		
+		contents = bookContext.getResource("ncc.html");
+		
+		encoding = obtainEncodingStringFromInputStream(contents);
 		Daisy202Book book = NccSpecification.readFromStream(contents);
-		System.out.println("Book: " + book.getTitle());
+		System.out.println("Book: " + book.getTitle() + ", encoding: " + encoding);
 		Navigator navigator = new Navigator(book);
 		while (navigator.hasNext()) {
 			Section section = (Section) navigator.next();
@@ -48,6 +44,19 @@ public class ProcessDaisy202Book {
 		}
 		
 		System.exit(0);
+	}
+
+	private static BookContext openBook(String filename) throws IOException {
+		BookContext bookContext;
+		
+		if (filename.endsWith(".zip")) {
+			bookContext = new ZippedBookContext(filename);
+		} else {
+			File directory = new File(filename);
+			bookContext = new FileSystemContext(directory.getParent());
+			directory = null;
+		}
+		return bookContext;
 	}
 
 }
