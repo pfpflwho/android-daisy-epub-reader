70ea731d4da522bf547609b7b07756f9fd65f38a
julianharty@gmail.com <julianharty@gmail.com@0d8aedb4-d056-11de-90e1-e379c62a6cce>
2012-03-27 20:44:50 +0000

    Adding more tests, and other changes flushed out by writing these tests. Note the addition of a custom Assertion to reduce code duplication.

--- branches/daisy-redesign/test/org/androiddaisyreader/model/MockAndroidAudioPlayerTest.java
+++ branches/daisy-redesign/test/org/androiddaisyreader/model/MockAndroidAudioPlayerTest.java
@@ -1,14 +1,13 @@
 package org.androiddaisyreader.model;
 
-import org.androiddaisyreader.AudioPlayer;
-import org.androiddaisyreader.mock.MockAndroidAudioPlayer;
-
 import junit.framework.TestCase;
+
+import org.androiddaisyreader.mock.MockAndroidAudioPlayer;
 
 public class MockAndroidAudioPlayerTest extends TestCase {
 
 	private static final String AUDIO_PLAYER_STATE_INCORRECT = "The Audio Player State is incorrect";
-	private MockAndroidAudioPlayer mockPlayer;
+	private MockAndroidAudioPlayer playertoTest;
 	private Audio initialSegment;
 	private Audio contiguousSegment;
 	private Audio gapAfterContiguousSegments;
@@ -17,23 +16,45 @@ public class MockAndroidAudioPlayerTest extends TestCase {
 
 	@Override
 	protected void setUp() {
-		mockPlayer = new MockAndroidAudioPlayer();
+		playertoTest = new MockAndroidAudioPlayer();
 		initialSegment = new Audio("initial", "file1.mp3", 0.0f, 1.234f);
 		contiguousSegment = new Audio("contiguous", "file1.mp3", 1.234f, 7.983f);
 		gapAfterContiguousSegments = new Audio("gap", "file1.mp3", 15.001f, 26.771f);
 		overlapWithInitialSegment = new Audio("overlap", "file1.mp3", 0.9f, 2.086f);
 		newAudioFilename = new Audio("newfile", "new.mp3", 0.0f, 11.589f);
+		playertoTest.playFileSegment(initialSegment);
 	}
 
 	public void testContiguousSegmentsAreRecognisedAsContiguous() {
-		mockPlayer.playFileSegment(initialSegment);
 		assertEquals(AUDIO_PLAYER_STATE_INCORRECT, 
 				AudioPlayerState.PLAY_NEW_FILE, 
-				mockPlayer.getInternalPlayerState());
-		mockPlayer.playFileSegment(contiguousSegment);
-		assertEquals(AUDIO_PLAYER_STATE_INCORRECT, 
-				AudioPlayerState.CONTINUE_PLAYING_EXISTING_FILE, 
-				mockPlayer.getInternalPlayerState());
+				playertoTest.getInternalPlayerState());
+		playertoTest.playFileSegment(contiguousSegment);
+		assertAudioPlayerStateIs(AudioPlayerState.CONTINUE_PLAYING_EXISTING_FILE);
 	}
 
+	public void testGapBetweenSegmentsIsDetected() {
+		playertoTest.playFileSegment(contiguousSegment);
+		playertoTest.playFileSegment(gapAfterContiguousSegments);
+		assertAudioPlayerStateIs(AudioPlayerState.GAP_BETWEEN_CONTENTS);
+	}
+	
+	public void testOverlappingSegmentsAreDetected() {
+		playertoTest.playFileSegment(overlapWithInitialSegment);
+		assertAudioPlayerStateIs(AudioPlayerState.OVERLAPPING_CONTENTS);
+	}
+
+	public void testNewAudioFileReplacesExistingOne() {
+		playertoTest.playFileSegment(newAudioFilename);
+		assertAudioPlayerStateIs(AudioPlayerState.PLAY_NEW_FILE);
+	}
+	
+	/**
+	 * Private helper method that asserts the AudioPlayer is in the expected state.
+	 * @param expectedState
+	 */
+	private void assertAudioPlayerStateIs(AudioPlayerState expectedState) {
+		assertEquals(AUDIO_PLAYER_STATE_INCORRECT, expectedState, 
+				playertoTest.getInternalPlayerState());
+	}
 }
